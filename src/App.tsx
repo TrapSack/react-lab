fdcf7ade6fc9054b8b7b22a4d6de9cb8e75d95b1
Aliaksandr Pratasau <mister.protasov@gmail.com>
2022-02-03 12:05:30 +0300

    feat(footer,project): rename component files to camel case, add hover styles on link in footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import { Component } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+// import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 
-export default function App() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+interface IError {
+  isError: boolean;
+}
+interface IState {
+  error: IError;
+}
+
+export default class App extends Component<unknown, IState> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      error: { isError: false },
+    };
+  }
+
+  static getDerivedStateFromError() {
+    return {
+      error: {
+        isError: true,
+      },
+    };
+  }
+
+  componentDidCatch() {
+    console.error("Error");
+    const navigate = useNavigate();
+    console.log("WORK");
+    navigate("/", { replace: true });
+  }
+
+  render() {
+    if (this.state.error.isError) return <div>Error</div>;
+    return (
+      // <ErrorBoundary>
+      <div>
+        <Header />
+        {/* {this.state.error.isError && <ErrorBoundary />} */}
+        <Outlet />
+        <Footer />
+      </div>
+      // </ErrorBoundary>
+    );
+  }
 }
