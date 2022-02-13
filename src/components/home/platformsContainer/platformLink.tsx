8c1ddcc6826e137072f97206565342d9b3f24738
Alex <mister.protasov@gmail.com>
2022-02-13 17:46:41 +0300

    feat(platform links): change ids to prop titles

--- src/components/home/platformsContainer/platformLink.tsx
+++ src/components/home/platformsContainer/platformLink.tsx
@@ -9,8 +9,8 @@ interface IPlatformLinkProps {
 
 export default function PlatformLink(props: IPlatformLinkProps) {
   return (
-    <Link to={`${products}/${props.link}`} className="platform-link" id="playstation">
-      <img src={props.cover} alt="" className="platform-link__logo" />
+    <Link to={`${products}/${props.link}`} className="platform-link" id={props.title}>
+      <img src={props.cover} alt={props.title} className="platform-link__logo" />
       <div className="platform-link__title">{props.title}</div>
     </Link>
   );
