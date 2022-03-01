d5786070e63161dde02c141b2a07207c22a67d16
Aliaksandr Pratasau <mister.protasov@gmail.com>
2022-03-01 12:03:02 +0300

    feat(product page): fix useless suspenses and errors

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,29 +1,27 @@
-// import GamesContainer from "@/elements/gamesContainer/gamesContainer";
-// import useLoader from "@/helpers/useLoader";
-// import { RootReducerType } from "@/redux/reducers/rootReducer";
-// import { useSelector } from "react-redux";
 import SearchField from "@/elements/searchField";
-// import GamesContainer from "@/elements/gamesContainer/gamesContainer";
-// import Loader from "@/elements/loader";
-// import { lazy } from "react";
-import "./products.scss";
 import React, { lazy } from "react";
+import "./products.scss";
 import Loader from "@/elements/loader";
+import useLoader from "@/helpers/useLoader";
+import { useSelector } from "react-redux";
+// import SortField from "./sortField";
+import { RootReducerType } from "@/redux/reducers/rootReducer";
+import GamesContainer from "@/elements/gamesContainer/gamesContainer";
 
-const GamesContainer = lazy(() => import("@/elements/gamesContainer/gamesContainer"));
+// const GamesContainer = lazy(() => import("@/elements/gamesContainer/gamesContainer"));
 const SortField = lazy(() => import("./sortField"));
 
 export default function Products() {
-  // const games = useSelector((state: RootReducerType) => state.games);
-  // const loaderState = useLoader(!!games.length);
+  const games = useSelector((state: RootReducerType) => state.games);
+  const loaderState = useLoader(!!games.length);
   return (
     <div className="products">
       <SearchField />
       <div className="products__main">
         <React.Suspense fallback={<Loader />}>
           <SortField />
-          {/* {loaderState ? <Loader /> : <GamesContainer />} */}
-          <GamesContainer />
+          {loaderState ? <Loader /> : <GamesContainer />}
+          {/* <GamesContainer /> */}
         </React.Suspense>
       </div>
     </div>
--- src/elements/gamesContainer/gamesContainer.tsx
+++ src/elements/gamesContainer/gamesContainer.tsx
@@ -1,96 +1,30 @@
-import axios from "axios";
-import React, { lazy, useEffect, useState } from "react";
-// import { RootReducerType } from "@/redux/reducers/rootReducer";
-// import { useSelector } from "react-redux";
-// import NoMatchesParagraph from "../noMatchesParagraph";
-import GameCard from "./gameCard";
+import { RootReducerType } from "@/redux/reducers/rootReducer";
+import React, { lazy } from "react";
+import { useSelector } from "react-redux";
 import Loader from "../loader";
-// import { IGame } from "@/redux/types/gamesTypes";
 
-async function getGames() {
-  const res = await axios.get("/api/getGames/", {
-    params: {
-      platform: "desktop",
-      genre: "",
-      age: "",
-      sortBy: "name",
-      orderBy: "ASC",
-    },
-  });
-  return res.data;
-}
-
-function wrap(res) {
-  console.log(res);
-  let status = "pending";
-  let result;
-  const suspender = res.then(
-    (r) => {
-      result = r;
-      status = "success";
-    },
-    (e) => {
-      result = e;
-      status = "error";
-    }
-  );
-  return {
-    read() {
-      if (status === "pending") {
-        throw suspender;
-      } else if (status === "error") {
-        throw result;
-      } else if (status === "success") {
-        return result;
-      }
-    },
-  };
-}
-
-function useResource() {
-  return {
-    games: wrap(getGames()),
-  };
-}
-
-const resource = useResource();
-const GameCard = lazy(() => import("@/elements/gamesContainer/gameCard"));
+const GameCard = lazy(() => import("./gameCard"));
 
 export default function GamesContainer(): JSX.Element {
   // const [gameComponentArray, setGameComponentArray] = useState([]);
-  // const gameComponentArray = useSelector((state: RootReducerType) => state.games).map((game) => (
-  //   <GameCard
-  //     key={game.id}
-  //     id={game.id}
-  //     name={game.name}
-  //     rating={game.rating}
-  //     price={game.price}
-  //     cover={game.cover}
-  //     description={game.description}
-  //     platforms={game.platforms}
-  //     releaseDate={game.releaseDate}
-  //     age={game.age}
-  //   />
-  // ));
-  // console.log(resource);
-  const gameComponentArray = resource.games
-    .read()
-    .map((game: IGame) => (
-      <GameCard
-        key={game.id}
-        id={game.id}
-        name={game.name}
-        rating={game.rating}
-        price={game.price}
-        cover={game.cover}
-        description={game.description}
-        platforms={game.platforms}
-        releaseDate={game.releaseDate}
-        age={game.age}
-      />
-    ));
-  console.log(gameComponentArray);
-  // const parsedData = await data.data;
-  // console.log(gameComponentArray);
-  return <div className="games-container">{gameComponentArray.length ? gameComponentArray : null}</div>;
+  const gameComponentArray = useSelector((state: RootReducerType) => state.games).map((game) => (
+    <GameCard
+      key={game.id}
+      id={game.id}
+      name={game.name}
+      rating={game.rating}
+      price={game.price}
+      cover={game.cover}
+      description={game.description}
+      platforms={game.platforms}
+      releaseDate={game.releaseDate}
+      age={game.age}
+    />
+  ));
+  // return <div className="games-container">{gameComponentArray.length ? gameComponentArray : null}</div>;
+  return (
+    <div className="games-container">
+      <React.Suspense fallback={<Loader />}>{gameComponentArray}</React.Suspense>
+    </div>
+  );
 }
