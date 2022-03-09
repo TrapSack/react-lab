ce12a5b14793c8096ded4707158f414562d8c257
Aliaksandr Pratasau <mister.protasov@gmail.com>
2022-03-09 16:53:54 +0300

    feat(tests): initial test commit

--- src/components/__tests__/gameCard.test.tsx
+++ src/components/__tests__/gameCard.test.tsx
@@ -1,22 +1,23 @@
-/**
- * @jest-environment jsdom
- */
-import ReactDOM from "react-dom";
-import App from "../../App";
+describe("React app", () => {
+  beforeAll(async () => {
+    await page.goto("http://localhost:8080/");
+    page.waitForTimeout(3000);
+  });
 
-describe("Home component tests", () => {
-  let container: HTMLDivElement;
-  beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-    ReactDOM.render(<App />, container);
+  it('should be titled "React App"', async () => {
+    await expect(page.title()).resolves.toMatch("React App");
   });
-  afterEach(() => {
-    document.body.removeChild(container);
-    container.remove();
-  });
-  it("Renders correctly initial document", () => {
-    const platforms = document.querySelector(".components-home-home__home__platforms");
-    expect(platforms).toBeTruthy();
+  // it("should show top products", async () => {
+  //   // const items = await page.$eval(".elements-elementStyles__games-container--home", (elem) => elem.childNodes);
+  //   // await expect(items.length).toBe(3);
+  //   await page.waitForTimeout(4000);
+  //   const item = await page.$eval(".elements-elementStyles__games-container--home", (itemd) => itemd);
+  //   console.log(item);
+  // });
+  it("should type", async () => {
+    const selector = `input[name="search-game"]`;
+    await page.click(selector);
+    await page.evaluate((sel) => document.querySelector(sel).click(), selector);
+    page.type(selector, "Overwatch");
   });
 });
