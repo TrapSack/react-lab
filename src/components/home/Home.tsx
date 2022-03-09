ea26edac75fa877f627774654bf5463fd4f6e524
Aliaksandr Pratasau <mister.protasov@gmail.com>
2022-03-09 12:22:13 +0300

    feat(home page top products): fix unneccessary api request if there are 3 products

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,18 @@
 import SearchField from "@/elements/searchField";
 import GamesContainer from "@/elements/gamesContainer/gamesContainer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducerType } from "@/redux/reducers/rootReducer";
+import { useEffect } from "react";
 import { getTopProducts } from "@/redux/actions/gamesActions";
 import Platforms from "./platformsContainer/platforms";
 import home from "./home.module.scss";
 
 export default function Home() {
   const dispatch = useDispatch();
-  dispatch(getTopProducts());
+  const games = useSelector((state: RootReducerType) => state.games);
+  useEffect(() => {
+    if (games.length !== 3) dispatch(getTopProducts());
+  }, []);
   return (
     <div className={home.home}>
       <SearchField topProducts />
